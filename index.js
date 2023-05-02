f8b4806e63e9deb261016e0d48eb42a1e91b357d
riaz9191 <riazahammed9191@gmail.com>
2023-05-02 23:02:28 +0600

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const cors = require('cors');
 const chefData = require('./chefData.json')
 const app = express();
+app.use(cors());
+
 app.get('/', (req, res)=>{
    
 })

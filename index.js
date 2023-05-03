e8d1a2ec3138b0bebc24ca28d3de64f0ada3bc65
riaz9191 <riazahammed9191@gmail.com>
2023-05-03 16:40:04 +0600

    chef id added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const chefData = require('./chefData.json')
+const port = process.env.PORT || 5000
 const app = express();
 app.use(cors());
 
@@ -10,7 +11,13 @@ app.get('/', (req, res)=>{
 app.get('/chefData', (req, res)=>{
     res.send(chefData)
 })
+app.get('/chefData/:id',(req, res)=>{
+    const id = req.params.id;
+    console.log(id)
+    const selected = news.find(n => n._id === id)
+    res.send(selected)
+})
 
-app.listen(5000,() => { 
+app.listen(port,() => { 
     console.log('server is running')
 });
